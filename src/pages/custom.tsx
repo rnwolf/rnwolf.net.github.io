560f58ae39cf900069bd72ab3a195507f549cadb
Rudiger Wolf <Rudiger.Wolf@ThroughputFocus.com>
2020-03-07 23:07:25 +0000

    Fix for Container

--- src/pages/custom.tsx
+++ src/pages/custom.tsx
@@ -2,6 +2,7 @@ import React, {FunctionComponent} from "react";
 import Layout from "@nehalist/gatsby-theme-nehalem/src/components/layout";
 import SEO from "@nehalist/gatsby-theme-nehalem/src/components/seo";
 import Subheader from "@nehalist/gatsby-theme-nehalem/src/components/subheader";
+mport {Container} from "@nehalist/gatsby-theme-nehalem/src/components/common";
 
 const CustomPage: FunctionComponent<{ location: Location }> = ({location}) => (
 	  <Layout bigHeader={false}>
@@ -10,7 +11,7 @@ const CustomPage: FunctionComponent<{ location: Location }> = ({location}) => (
 	      title={`Page title`}
 	    />
     	  <Subheader title={`Page title`} subtitle={`Something else`} />
-	    <div>
+	    <Container>
 		<p>I'll send out a newsletter containing interesting updates every couple of months. Let me know if you would like a copy.</p> 
       		<form accept-charset="UTF-8" action="https://www.formbackend.com/f/1423c13e3fbcba3b" method="POST">
   			<label for="name">Name</label>
@@ -22,7 +23,7 @@ const CustomPage: FunctionComponent<{ location: Location }> = ({location}) => (
   			<button type="submit">Submit</button>
 		</form>
     
-	    </div>
+	    </Container>
 	</Layout>
 );
 
