829734dfe7e3dcd89182342274fdbf1fb6a8cf51
Rudiger Wolf <Rudiger.Wolf@ThroughputFocus.com>
2020-03-07 17:36:36 +0000

    Remove Container

--- src/pages/custom.tsx
+++ src/pages/custom.tsx
@@ -1,7 +1,6 @@
 import React, {FunctionComponent} from "react";
 import Layout from "@nehalist/gatsby-theme-nehalem/src/components/layout";
 import SEO from "@nehalist/gatsby-theme-nehalem/src/components/seo";
-import Container from "@nehalist/gatsby-theme-nehalem/src/components/common";
 
 const CustomPage: FunctionComponent<{ location: Location }> = ({location}) => (
 	  <Layout bigHeader={false}>
@@ -9,9 +8,9 @@ const CustomPage: FunctionComponent<{ location: Location }> = ({location}) => (
 	      location={location}
 	      title={`Page title`}
 	    />
-	    <Container>
+	    <div>
 	      Your content    
-	    </Container>
+	    </div>
 	</Layout>
 );
 
