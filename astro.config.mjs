4c07bb17856a81561523b3c0a92525b48840e626
Rudiger Wolf <Rudiger.Wolf@ThroughputFocus.com>
2024-05-17 21:43:28 +0100

    reading time for post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.rnwolf.net',
-  integrations: [mdx()]
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
