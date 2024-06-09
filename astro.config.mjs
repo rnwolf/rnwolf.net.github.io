8fe688cb35fb0ba7de3a594193b434cbd51926f8
Rudiger Wolf <Rudiger.Wolf@ThroughputFocus.com>
2024-06-09 14:58:06 +0100

    Back to basics for npm github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
-import { remarkReadingTime } from './remark-reading-time.mjs';
-import remarkToc from 'remark-toc';
-import rehypeExternalLinks from 'rehype-external-links';
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
@@ -10,16 +6,5 @@ export default defineConfig({
   site: 'https://www.rnwolf.net',
   integrations: [mdx()],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
-    remarkPlugins: [remarkToc],
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          content: { type: 'text', value: ' 🔗' }
-        }
-      ],
-    ]
-
   },
 });
